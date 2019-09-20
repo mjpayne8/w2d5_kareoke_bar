d125f7e3830243502cbf11c167bdfd90036f402d
Mark Payne <mjpayne8@gmail.com>
2019-09-20 13:39:38 +0100

    tests added on add_guest if enough money to room_spec

--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -8,7 +8,9 @@ class TestRoom  < MiniTest::Test
   def setup()
     @song = Song.new("Gangnam Style")
     @guest = Guest.new("Mark", @song, 50)
+    @guest_2 = Guest.new("James", @song, 50)
     @room = Room.new(1, 10, 2)
+    @room_2 = Room.new(1, 100, 1)
   end
 
   def test_room_id__returns_1()
@@ -23,8 +25,24 @@ class TestRoom  < MiniTest::Test
     assert_equal(0, @room.number_of_guests)
   end
 
-  #def test_add_song
+  def test_add_guest__enough_money__gets_added()
+    @room.add_guest(@guest)
+    assert_equal(1, @room.number_of_guests())
+  end
+
+  def test_add_guest__not_enough_money_not_added()
+    @room_2.add_guest(@guest)
+    assert_equal(0, @room_2.number_of_guests())
+  end
+
+  # def test_add_song__guest_not_in_room_no_change_to_song_list()
   #  @room.add_song(@guest, @song)
-  #end
+  #  assert_equal(0, @room.number_of_songs())
+  # end
+  #
+  # def test_add_song__guest_in_room_add_to_song_list()
+  #  @room.add_song(@guest, @song)
+  #  assert_equal(1, @room.number_of_songs())
+  # end
 
 end
