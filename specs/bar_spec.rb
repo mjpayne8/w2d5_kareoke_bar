74215d64c15c039101ac4b458426fe7f17b34903
Mark Payne <mjpayne8@gmail.com>
2019-09-22 15:31:50 +0100

    files put back to original(uncommented) after screen shots for pda

--- specs/bar_spec.rb
+++ specs/bar_spec.rb
@@ -15,15 +15,15 @@ class TestBar < MiniTest::Test
     @bar = Bar.new([@room])
   end
 
-  # def test_get_tab__returns_0()
-  #   assert_equal(0, @bar.get_tab(@room))
-  # end
-  #
-  # def test_add_to_tab__guest_in_room_and_enough_money()
-  #   @bar.add_to_tab(@room, @guest, 10)
-  #   assert_equal(10, @bar.get_tab(@room))
-  #   assert_equal(30, @guest.wallet_amount())
-  # end
+  def test_get_tab__returns_0()
+    assert_equal(0, @bar.get_tab(@room))
+  end
+
+  def test_add_to_tab__guest_in_room_and_enough_money()
+    @bar.add_to_tab(@room, @guest, 10)
+    assert_equal(10, @bar.get_tab(@room))
+    assert_equal(30, @guest.wallet_amount())
+  end
 
   def test_add_to_tab__guest_not_in_room()
     @room.remove_guest(@guest)
